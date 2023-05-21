ed88b26665fedfafd890dcc55ea5275e3c8dd7db
HaKien <nguyenhakien99@gmail.com>
2023-05-22 00:58:51 +0700

    done

--- index.js
+++ index.js
@@ -83,38 +83,79 @@ document.getElementById("btn2").onclick = bai2;
 // BÀI 3
 
 function taxCalculation(yearIncomeInput) {
+
     let taxPay = 0;
     if (yearIncomeInput <= 60e+6) {
-        taxPay = 0.05
-    } else if (yearIncomeInput <= 120e+6) {
-        taxPay = 0.1
+        taxPay = 0.05;
+    } else if (yearIncomeInput <= 12e+6) {
+        taxPay = 0.1;
     } else if (yearIncomeInput <= 210e+6) {
-        taxPay = 0.15
+        taxPay = 0.15;
     } else if (yearIncomeInput <= 384e+6) {
-        taxPay = 0.2
+        taxPay = 0.2;
     } else if (yearIncomeInput <= 624e+6) {
-        taxPay = 0.25
+        taxPay = 0.25;
     } else if (yearIncomeInput <= 960e+6) {
-        taxPay = 0.3
+        taxPay = 0.3;
     } else {
-        taxPay = 0.35
+        taxPay = 0.35;
     }
     return taxPay;
 }
 
 function bai3() {
     var userName = document.getElementById("nameUser3").value;
-    var income = +document.getElementById("yearIncomeInput").value;
+    var yearIncomeInput = parseFloat(document.getElementById("yearIncomeInput").value);
     var dependents = +document.getElementById("dependentsInput").value;
 
     var tax = taxCalculation(yearIncomeInput);
 
     var total;
 
-    total = income * tax - dependents * 16e+5;
+    total = yearIncomeInput * tax - dependents * 1.6e+6;
 
     document.getElementById("name3").innerHTML = "Tên: " + userName + " |";
     document.getElementById("result3").innerHTML = " Tiền thuế thu nhập cá nhân: " + Intl.NumberFormat('vn-VN',).format(total);
-    // Intl.NumberFormat('vn-VN',).format(total);
 }
 document.getElementById("btn3").onclick = bai3;
+
+///////////////////////////////////////////////////////////////////
+
+// BÀI 4
+
+
+function bai4() {
+
+    var customerType = document.getElementById("customerType").value;
+    var idUser = document.getElementById("customerIDInput").value;
+    var chanelNum = +document.getElementById("chanelInput").value;
+    var linkNum = +document.getElementById("linkInput").value;
+
+    var billNormal = 4.5;
+    var serviceNormal = 20.5;
+    var chanelVip = 7.5
+
+
+    var billEnterprise = 15;
+    var chanelVipEnterprise = 50;
+    var serviceEnterprise = 75;
+    var total;
+
+    if (customerType == 1) {
+        total = billNormal + serviceNormal + chanelNum * chanelVip;
+
+    } else if (customerType == 2 && linkNum <= 10) {
+        total = billEnterprise + serviceEnterprise + chanelNum * chanelVipEnterprise;
+
+    } else if (customerType == 2 && linkNum > 10) {
+        total = billEnterprise + serviceEnterprise + ((linkNum - 10) * 5) + chanelNum * chanelVipEnterprise;
+    } else {
+        document.getElementById("id").innerHTML = "Hãy chọn loại khách hàng";
+    }
+    document.getElementById("id").innerHTML = "Mã khách hàng" + idUser + " | ";
+    document.getElementById("result4").innerHTML = "Tiền cáp " +
+        Intl.NumberFormat('de-DE', { style: 'currency', currency: 'USA' }).format(total);
+}
+
+document.getElementById("btn4").onclick = bai4;
+
